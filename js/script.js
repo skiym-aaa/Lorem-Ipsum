78b45eb3ea7c46de3040b28933d595f6780b8071
skiym-aaa <y.sakiyama3216@gmail.com>
2020-03-15 16:26:34 +0900

    [Update] Hamburger

--- js/script.js
+++ js/script.js
@@ -5,5 +5,16 @@ $(function() {
     $('#sp-menu').fadeToggle();
     return false;
   });
- 
+  
+  $('.menu a[href]').on('click', function() {
+    $('.menu-trigger').toggleClass('active');
+    $('#sp-menu').fadeToggle();
+  });
+
+  $('#back a').on('click',function(){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+      return false;
+  });
  });
